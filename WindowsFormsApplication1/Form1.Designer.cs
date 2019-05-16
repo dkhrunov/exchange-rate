062fb3e709f958a84358c817c6ad334d3795f426
therealpanda98 <therealpanda@mail.ru>
2019-05-16 15:44:58 +0300

    Ready application
    The main functions are made: 1) display the rate of the selected currency on a specific date; 2) the choice of the period of change in the price of the currency and the display on the chart.

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker3 = new System.Windows.Forms.DateTimePicker();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(129, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(129, 58);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "USD";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "EUR";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(99, 471);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(311, 100);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Курс доллара и евро";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(96, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Выбрать дату";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(246, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(164, 21);
+            this.comboBox1.TabIndex = 9;
+            this.comboBox1.SelectedValueChanged += new System.EventHandler(this.comboBox1_SelectedValueChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(96, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Выбор валюты";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(246, 65);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(164, 20);
+            this.textBox4.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(96, 68);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Курс валюты";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(271, 91);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Узнать курс";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            this.chart1.Location = new System.Drawing.Point(12, 165);
+            this.chart1.Name = "chart1";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.Name = "Series1";
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(484, 289);
+            this.chart1.TabIndex = 15;
+            this.chart1.Text = "chart1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(422, 136);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(74, 21);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Показать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 144);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(20, 13);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "От";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(218, 144);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(22, 13);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "До";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(246, 42);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(164, 20);
+            this.dateTimePicker1.TabIndex = 23;
+            this.dateTimePicker1.CloseUp += new System.EventHandler(this.dateTimePicker1_CloseUp);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(38, 138);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(164, 20);
+            this.dateTimePicker2.TabIndex = 24;
+            this.dateTimePicker2.CloseUp += new System.EventHandler(this.dateTimePicker2_CloseUp);
+            // 
+            // dateTimePicker3
+            // 
+            this.dateTimePicker3.Location = new System.Drawing.Point(246, 138);
+            this.dateTimePicker3.Name = "dateTimePicker3";
+            this.dateTimePicker3.Size = new System.Drawing.Size(164, 20);
+            this.dateTimePicker3.TabIndex = 25;
+            this.dateTimePicker3.CloseUp += new System.EventHandler(this.dateTimePicker3_CloseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(508, 583);
+            this.Controls.Add(this.dateTimePicker3);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Курс валют ЦБ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker3;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, string> valutes = new Dictionary<string, string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MaximizeBox = false;
+            chart1.Visible = false;
+
+            string date = DateTime.Now.ToString("dd/MM/yyyy").Replace('.', '/');
+            string url = "http://www.cbr.ru/scripts/XML_daily.asp?date_req=" + date;
+
+            XmlTextReader reader = new XmlTextReader(url);
+
+            while (reader.Read())
+            {
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element:
+
+                        if (reader.Name == "Valute")
+                        {
+                            if (reader.HasAttributes)
+                            {
+                                while (reader.MoveToNextAttribute())
+                                {
+                                    if (reader.Name == "ID")
+                                    {
+                                        string code = reader.Value;
+                                        while (reader.Name != "CharCode")
+                                        {
+                                            reader.Read();
+                                        }
+                                        reader.Read();
+
+                                        valutes.Add(code, reader.Value);
+                                    }
+                                }
+                            }
+                        }
+
+                        break;
+                }
+            }
+            reader.Close();
+
+            foreach (KeyValuePair<string, string> keyValue in valutes)
+            {
+                comboBox1.Items.Add(keyValue.Value);
+            }
+           
+            // Курс USD и EUR на сегодня
+            groupBox1.Text = "Курс доллара и евро на " + date;
+
+            string urlDaily = "http://www.cbr.ru/scripts/XML_daily.asp?date_req=" + date;
+            XmlTextReader readerDaily = new XmlTextReader(urlDaily);
+
+            while (readerDaily.Read())
+            {
+                switch (readerDaily.NodeType)
+                {
+                    case XmlNodeType.Element:
+
+                        if (readerDaily.Name == "Valute")
+                        {
+                            if (readerDaily.HasAttributes)
+                            {
+                                while (readerDaily.MoveToNextAttribute())
+                                {
+                                    // USD
+                                    if (readerDaily.Name == "ID")
+                                    {
+                                        if (readerDaily.Value == "R01235")
+                                        {
+                                            while (readerDaily.Name != "Value")
+                                            {
+                                                readerDaily.Read();
+                                            }
+
+                                            readerDaily.Read();
+                                            textBox1.Text = readerDaily.Value;
+                                        }
+                                    }
+
+                                    //EUR
+                                    if (readerDaily.Name == "ID")
+                                    {
+                                        if (readerDaily.Value == "R01239")
+                                        {
+                                            while (readerDaily.Name != "Value")
+                                            {
+                                                readerDaily.Read();
+                                            }
+
+                                            readerDaily.Read();
+                                            textBox2.Text = readerDaily.Value;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+                        break;
+                }
+            }
+            readerDaily.Close();
+        }
+
+        private void compareDateToPresent(DateTimePicker date)
+        {
+
+            if (DateTime.Compare(date.Value.Date, DateTime.Now.Date) > 0)
+            {
+                date.Value = DateTime.Now;
+                MessageBox.Show("Я не умею предсказывать курс валют!");
+            }
+        }
+
+        private void comboBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            string url = "http://www.cbr.ru/scripts/XML_daily.asp?date_req=" + 
+                          dateTimePicker1.Value.ToString("dd/MM/yyyy").Replace('.', '/');
+
+            XmlTextReader reader = new XmlTextReader(url);
+
+            while (reader.Read())
+            {
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element:
+                        if (reader.Name == "CharCode")
+                        {
+                            reader.Read();
+                            if (reader.Value == comboBox1.Text)
+                            {
+                                while (reader.Name != "Nominal")
+                                {
+                                    reader.Read();
+                                }
+                                reader.Read();
+                                label5.Text = "Курс " + reader.Value + " " + comboBox1.Text + ":";
+                            }
+                        }
+                        break;
+                }
+            }
+            reader.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "" || dateTimePicker1.Value.ToString() == "")
+            {
+                MessageBox.Show("Заполните поля выше!");
+            }
+            else
+            {
+                string url = "http://www.cbr.ru/scripts/XML_daily.asp?date_req=" +
+                              dateTimePicker1.Value.ToString("dd/MM/yyyy").Replace('.', '/');
+
+                XmlTextReader reader = new XmlTextReader(url);
+
+                while (reader.Read())
+                {
+                    switch (reader.NodeType)
+                    {
+                        case XmlNodeType.Element:
+                            if (reader.Name == "CharCode")
+                            {
+                                reader.Read();
+                                if (reader.Value == comboBox1.Text)
+                                {
+                                    while (reader.Name != "Value")
+                                    {
+                                        reader.Read();
+                                    }
+                                    reader.Read();
+                                    textBox4.Text = reader.Value + " руб.";
+                                }
+                            }
+                            break;
+                    }
+                }
+                reader.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bool btnCliked = false;
+
+            TimeSpan timeSpan = dateTimePicker3.Value.Date - dateTimePicker2.Value.Date;
+            if (timeSpan.TotalDays < 6)
+            {
+                MessageBox.Show("Минимальный период изменения цен на валюту 6 дней!");
+            }
+            else if (comboBox1.Text == "")
+            {
+                MessageBox.Show("Выберите валюту!");
+            }
+            else
+            {
+                btnCliked = true;
+
+                string valuteCode = "";
+                Dictionary<string, double> prices = new Dictionary<string, double>();
+
+                foreach (KeyValuePair<string, string> keyValue in valutes)
+                {
+                    if (keyValue.Value == comboBox1.Text)
+                    {
+                        valuteCode = keyValue.Key;
+                    }
+                }
+
+                string url = "http://www.cbr.ru/scripts/XML_dynamic.asp?date_req1=" + 
+                               dateTimePicker2.Value.ToString("dd/MM/yyyy").Replace('.', '/') +
+                              "&date_req2=" + dateTimePicker3.Value.ToString("dd/MM/yyyy").Replace('.', '/') + 
+                              "&VAL_NM_RQ=" + valuteCode;
+
+                XmlTextReader reader = new XmlTextReader(url);
+
+                while (reader.Read())
+                {
+                    switch (reader.NodeType)
+                    {
+                        case XmlNodeType.Element:
+
+                            if (reader.Name == "Record")
+                            {
+                                if (reader.HasAttributes)
+                                {
+                                    while (reader.MoveToNextAttribute())
+                                    {
+                                        if (reader.Name == "Date")
+                                        {
+                                            string date = reader.Value;
+                                            while (reader.Name != "Value")
+                                            {
+                                                reader.Read();
+                                            }
+                                            reader.Read();
+
+                                            prices.Add(date, Convert.ToDouble(reader.Value));
+                                        }
+                                    }
+                                }
+                            }
+
+                            break;
+                    }
+                }
+                reader.Close();
+
+                if (btnCliked)
+                {
+                    chart1.Series["Series1"].Points.Clear();
+
+                }
+
+                double min = prices.Min(s => s.Value);
+                double max = prices.Max(s => s.Value);
+                chart1.ChartAreas[0].AxisY.Minimum = min;
+                chart1.ChartAreas[0].AxisY.Maximum = max;
+
+                foreach (KeyValuePair<string, double> keyValue in prices)
+                {
+                    chart1.Series["Series1"].Points.AddXY(keyValue.Key, keyValue.Value);
+                }
+                chart1.Visible = true;
+            }
+        }
+
+        private void dateTimePicker1_CloseUp(object sender, EventArgs e)
+        {
+            compareDateToPresent(dateTimePicker1);
+        }
+
+        private void dateTimePicker2_CloseUp(object sender, EventArgs e)
+        {
+            compareDateToPresent(dateTimePicker2);
+        }
+
+        private void dateTimePicker3_CloseUp(object sender, EventArgs e)
+        {
+            compareDateToPresent(dateTimePicker3);
+
+            if (DateTime.Compare(dateTimePicker2.Value.Date, dateTimePicker3.Value.Date) > 0)
+            {
+                dateTimePicker3.Value = DateTime.Now;
+                MessageBox.Show("Ошибка в выборе периода!\nВторая дата должны быть старше первой!\nВыберите корректный период.");
+            }
+        }
+    }
+}
